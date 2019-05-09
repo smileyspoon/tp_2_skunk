35ffe32d546e7ae055c87b766aea438752b4a5d8
Sheilaher07 <her09912@stthomas.edu>
2019-05-08 23:31:17 -0500

    added test cases for gameTest

--- tp_1_skunk-/test/gameTest.java
+++ tp_1_skunk-/test/gameTest.java
@@ -16,23 +16,71 @@ public class gameTest {
 	@Test
 	public void newPlayerTest() throws Exception {
 		Game game = new Game();
-		
+
 		game.newPlayer(1);
 		game.newPlayer(2);
 		game.newPlayer(3);
-				
-		assertEquals(3,game.totalPlayers());	
+
+		assertEquals(3,game.totalPlayers());
 	}
-	
+
 	@SuppressWarnings("unused")
 	@Test
 	public void getPlayerTest() throws Exception {
 		Player player = new Player(1);
 		Game game = new Game();
-		game.getPlayer(1).setName("Jim");;		
-		
+		game.getPlayer(1).setName("Jim");
+
 		assertEquals ("Jim",game.getPlayer(1).getName());
-				
+
 	}
+
+	@Test
+	public void getKittyTest() {
+		Game game = new Game();
+		Player player = new Player(1);
+		game.setKitty(player, 5);
+		assertEquals((-5),game.getKitty());
+
+	}
+	@Test
+	public void setKittyTest() {
+		Game game = new Game();
+		Player player = new Player(1);
+		game.setKitty(player, 17);
+		assertEquals(17,17);
+
+	}
+	@Test
+	public void resetKittyTest() {
+		Game game = new Game();
+		Player player = new Player(1);
+		game.setKitty(player,15);
+		game.resetKitty();
+
+		assertEquals(0,0);
+	}
+
+////	@Test
+////	public void eliminatePlayerTest () throws Exception {
+////		Game game = new Game();
+////		@SuppressWarnings("unused")
+////		Player player = new Player(1);
+////		game.getPlayer(1);
+////		game.eliminatePlayer(1);
+////
+////		assertEquals(1, player.size());
+////
+////	}
+//
+//	@Test
+//	public void getEliminatedPlayers() throws Exception{
+//
+//	}
+
+	//Testing
+
+
+
 
 }
